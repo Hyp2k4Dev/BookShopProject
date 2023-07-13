2de7cd4ab256c6446d317bf4d8ecdb60318a9c98
Hiep <hiephoang1752004@gmail.com>
2023-07-13 17:43:40 +0700

    update by Hiep

--- ThreeLayerLib/Persistence/Book.cs
+++ ThreeLayerLib/Persistence/Book.cs
@@ -4,7 +4,13 @@ public class Book
 {
     public int BookID { get; set; }
     public required string Title { get; set; }
-    public Auhtor Author { get; set; }
+
+    public Author? AuthorName { get; set; }
+    public Publisher? PublisherName { get; set; }
+    public int PublishingYear { get; set; }
+
+    public Author? Author { get; set; }
+
     public decimal Price { get; set; }
     public int Status { get; set; }
 
