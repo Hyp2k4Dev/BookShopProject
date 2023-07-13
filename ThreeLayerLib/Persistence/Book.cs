07bee96d027031384a896a6ce030ef14aaa9a23a
Tuan <tuantd1801@gmail.com>
2023-07-13 17:31:48 +0700

    accept

--- ThreeLayerLib/Persistence/Book.cs
+++ ThreeLayerLib/Persistence/Book.cs
@@ -4,7 +4,7 @@ public class Book
 {
     public int BookID { get; set; }
     public required string Title { get; set; }
-    public Auhtor Author { get; set; }
+    public Auhtor? Author { get; set; }
     public decimal Price { get; set; }
 
 }
