ccf707427149d8f802c7190229838a6fc74157d0
Tuan <tuantd1801@gmail.com>
2023-07-16 17:10:28 +0700

    update

--- ThreeLayerLib/Persistence/Book.cs
+++ ThreeLayerLib/Persistence/Book.cs
@@ -5,9 +5,10 @@ public class Book
     public int BookID { get; set; }
     public int ISBN { get; set; }
     public string? Title { get; set; }
-    public Author? AuthorID { get; set; }
-    public Publisher? PublisherID { get; set; }
+    public Author? BookAuthor { get; set; }
+    public Publisher? BookPublisher { get; set; }
     public int PublishYear { get; set; }
     public decimal Price { get; set; }
+    public int Quantity { get; set; }
     public int Status { get; set; }
 }
